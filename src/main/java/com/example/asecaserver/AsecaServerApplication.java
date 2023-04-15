49f6eeb285aa44723dbb4fdf53fe1a97e841733a
Agustin <agustin.titotto@ing.austral.edu.ar>
2023-04-15 15:52:32 -0300

    initial setup

---
+++ src/main/java/com/example/asecaserver/AsecaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.asecaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsecaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsecaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asecaserver/AsecaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asecaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsecaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
